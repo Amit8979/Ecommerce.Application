c80175bfbba3fef43cd64e528c93fe8bf4d296d9
Amit Gupta <amitguptaagra8979@gmail.com>
2025-08-04 13:23:13 +0530

    Added the services and the created the service collection extension file

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -1,6 +1,24 @@
+using eCommerce.Core;
+using eCommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddInfrastructure();
+builder.Services.AddCoreService();
+
+// Add controllers
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Routing.
+app.UseRouting();
+
+// Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Controller Routes.
+app.MapControllers();
 
 app.Run();
---
+++ eCommerceSolution.UsersService/eCommerce.Core/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Core
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddCoreService(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
